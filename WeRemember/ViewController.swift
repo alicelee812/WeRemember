a0f69f5baec2fbd86e087fc60575075b02ec2509
calvinlee417 <calvinlee417@gmail.com>
2022-11-14 10:04:03 +0800

    Initial Commit

---
+++ WeRemember/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeRemember
+//
+//  Created by Alice on 2022/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
